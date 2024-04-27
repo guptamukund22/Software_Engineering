6ea64d9763b1c184c52fd624d502623a6459ef1e
ssp-8 <sumeetpatil20004@gmail.com>
2024-04-27 13:53:50 +0530

    Deployment of server done

--- paakshaala/backend/paakshala/paakshala/urls.py
+++ paakshaala/backend/paakshala/paakshala/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apis/v1/',include('apis.urls')),
 ]
+
+urlpatterns+=staticfiles_urlpatterns()
